3b1e1e48183f56f15be611e24ad005eac7d4b65e
bikhoa142007-hash <bikhoa142007@gmail.com>
2026-05-24 16:53:24 +0700

    commit

---
+++ app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import streamlit_drawable_canvas as drawable_canvas
+
+@st.cache_resource
+def load_trained_model():
+    return tf.keras.models.load_model("emoji_dense_model.keras")
+
+model = load_trained_model()
+Emoji_name = [
+    "Grinning Face",
+    "Grinning Face With Big Eyes",
+    "Grinning Face With Smiling Eyes",
+    "Beaming Face With Smiling Eyes",
+    "Grinning Squinting Face",
+    "Grinning Face With Sweat",
+    "Rolling On The Floor Laughing",
+    "Face With Tears Of Joy",
+    "Slightly Smiling Face",
+    "Upside-down Face",
+    "Melting Face",
+    "Winking Face",
+    "Smiling Face With Smiling Eyes",
+    "Smiling Face With Halo",
+    "Smiling Face With Hearts",
+    "Smiling Face With Heart-eyes",
+    "Star-struck",
+    "Face Blowing A Kiss",
+    "Kissing Face",
+    "Smiling Face",
+    "Kissing Face With Closed Eyes",
+    "Kissing Face With Smiling Eyes",
+    "Smiling Face With Tear",
+    "Face Savoring Food",
+    "Face With Tongue",
+    "Winking Face With Tongue",
+    "Zany Face",
+    "Squinting Face With Tongue",
+    "Money-mouth Face",
+    "Smiling Face With Open Hands"
+]
+def app():
+    st.title("App Emoji")
+    tab1, tab2, tab3 = st.tabs(["Bảng vẽ", "Ô nhập người dùng", "Bảng kết quả"])
+    with tab1:
+        st.header("Bảng vẽ")
+        st.write("Đây là nơi bạn có thể vẽ hình ảnh của mình.")
+        # Art tool
+        canvas_result = drawable_canvas.st_canvas(
+            fill_color="rgba(255, 165, 0, 0.3)",
+            stroke_width=2,
+            stroke_color="#000000",
+            background_color="#ffffff",
+            height=400,
+            width=400,
+            drawing_mode="freedraw",
+            key="canvas"
+        )
+    with tab2:
+        st.header("Ô nhập người dùng")
+        st.write("Nhập thông tin của bạn vào đây.")
+        name = st.text_input("Tên của bạn")
+        age = st.number_input("Tuổi của bạn", min_value=0, max_value=120)
+        button = st.button("Dự đoán hình vẽ")
+    with tab3:
+        st.header("Kết quả")
--- database.py
+++ database.py
@@ -1,12 +1,11 @@
 import random 
 from keras.src.ops import softmax
-from sklearn.model_selection import train_test_split
 import streamlit as st
 import pandas as pd
 import json
 import streamlit_drawable_canvas as drawable_canvas
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Input, Dropout
 from keras.utils import to_categorical
 from keras.utils import load_img, img_to_array
 from PIL import Image
@@ -48,12 +47,15 @@ def get_data(images_folder, file_csv, num_classes=30, variants_class=50):
     df.to_csv(file_csv, index=False)
 # Train function
 class EmojiModel():
-    def __init__(self, model):
+    def __init__(self):
         self.model = None
     def build_model(self):
         self.model = Sequential()
+        self.model.add(Input(shape=(1024,)))
         self.model.add(Dense(512,activation='relu'))
+        self.model.add(Dropout(0.3))
         self.model.add(Dense(256,activation='relu'))
+        self.model.add(Dropout(0.3))
         self.model.add(Dense(30,activation='softmax'))
         self.model.summary()
     def train_model(self, x_train, y_train):
@@ -61,45 +63,16 @@ class EmojiModel():
             self.build_model()
         self.model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
         self.model.fit(x_train, y_train, epochs=30, batch_size=64)
-
 # Main page
-def app():
-    tab1, tab2, tab3 = st.tabs(["Bảng vẽ", "Ô nhập người dùng", "Bảng kết quả"])
-    with tab1:
-        st.header("Bảng vẽ")
-        st.write("Đây là nơi bạn có thể vẽ hình ảnh của mình.")
-        # Art tool
-        canvas_result = drawable_canvas.st_canvas(
-            fill_color="rgba(255, 165, 0, 0.3)",
-            stroke_width=2,
-            stroke_color="#000000",
-            background_color="#ffffff",
-            height=400,
-            width=400,
-            drawing_mode="freedraw",
-            key="canvas"
-        )
-    with tab2:
-        st.header("Ô nhập người dùng")
-        st.write("Nhập thông tin của bạn vào đây.")
-        name = st.text_input("Tên của bạn")
-        age = st.number_input("Tuổi của bạn", min_value=0, max_value=120)
-        button = st.button("Dự đoán hình vẽ")
-    with tab3:
-        st.header("Kết quả")
-        if button:
-            if name.strip() != 0 and age > 0:
-                col1, col2, col3, col4 = st.columns(4)
-                with col1:
-                    st.subheader("Tên")
-                    st.write(name)
-                with col2:
-                    st.subheader("Tuổi")
-                    st.write(age)
-                with col3:
-                    #
-                    st.write("Đã xử lý hình ảnh")
-            else:
-                st.warning("Vui lòng nhập tên tuổi trước khi dự đoán hình ảnh")
-        else:
-            st.info("Vui lòng vẽ và nhập đầy đủ thông tin trước khi dự đoán hình ảnh")
+if __name__ == "__main__":
+    emoji = "./emoji"
+    File_csv = "file_csv.csv"
+    df = pd.read_csv("File_csv")
+    y_train = df['label'].values
+    x_train = df.drop(columns=['label']).values
+    x_train_scaled = x_train.astype('float32')/255.0
+    y_train_scaled = to_categorical(y_train, num_classes=30)
+    my_emoji = EmojiModel()
+    my_emoji.train_model(x_train_scaled, y_train_scaled)
+    my_emoji.model.save("emoji_dense_model.keras")
+
